dbda016e94c878bb42ea33fbb763fb80f065934e
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2018-10-26 14:26:05 +0100

    aula 19

---
+++ aula19-b4-addBook-to-Bundle/test/bundles-test.js
@@ -0,0 +1,61 @@
+'use strict'
+
+const assert = require('assert')
+const bundles = require('./../lib/bundles.js')
+
+const es = {
+    host: 'localhost',
+    port: '9200',
+    books_index: 'books',
+    bundles_index: 'bundles'
+}
+
+describe('Bundles API', () => {
+
+    it('should create a bundles service object!', done => {
+        const service = bundles.init(es)
+        assert.equal(service.urlBooks, 'http://localhost:9200/books/book/')
+        assert.equal(service.urlBundles, 'http://localhost:9200/bundles/bundle/')
+        done()
+    })
+
+    it('should create a new bundle object and get it and delete it!', done => {
+        const service = bundles.init(es)
+        service.create('action', (err, data) => {
+            if(!data._id)
+                assert.fail('Missing _id on bundle creation')
+            const id = data._id
+            service.get(id, (err, bundle) => {
+                assert.equal(bundle.name, 'action')
+                service.delete(id, (err) => {
+                    if(err) assert.fail('bundle delete failed!')
+                    service.get(id, (err) => {
+                        if(!err) assert.fail('bundle not deleted')
+                        done()
+                    })
+                })
+            })
+        })
+    })
+    it('should create a new bundle object and add it a book!', done => {
+        const service = bundles.init(es)
+        service.create('action', (err, data) => {
+            if(!data._id)
+                assert.fail('Missing _id on bundle creation')
+            const id = data._id
+            service.addBook(id, 'pg18381', (err) => {
+                service.get(id, (err, bundle) => {
+                    assert.equal(bundle.name, 'action')
+                    assert.equal(bundle.books[0].title, 'De Lotgevallen van Tom Sawyer')
+                    service.delete(id, (err) => {
+                        if(err) assert.fail('bundle delete failed!')
+                        service.get(id, (err) => {
+                            if(!err) assert.fail('bundle not deleted')
+                            done()
+                        })
+                    })
+                })            
+            })
+        })
+    })
+})
